8bef29351eed26b4fd23d8887e05c179597270c6
Emanuel <emanuel.gui.santana@gmail.com>
2023-11-12 20:44:28 -0300

    alterando nome da aplicação no arquivo settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'midia',
+    'midia.apps.MidiaConfig',
 ]
 
 MIDDLEWARE = [
