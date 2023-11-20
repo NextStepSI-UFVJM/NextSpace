092fcfa615cecf2361fb24085d4a07e88740915e
Emanuel <emanuel.gui.santana@gmail.com>
2023-11-20 13:56:01 -0300

    Adicionando models de midia na bbranch development

--- midia/models.py
+++ midia/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Midia(models.Model):
+
+  Id = models.AutoField(primary_key=True)
+
+
+  EVENTOS = 'Eventos'
+  VIAGENS = 'Viagens'
+  HAPPY_HOUR = 'Happy Hour'
+
+  CATEGORIA_CHOICES = [
+    (EVENTOS, 'Eventos'),
+    (VIAGENS, 'Viagens'),
+    (HAPPY_HOUR, 'Happy Hour'),
+  ]
+
+  Categoria = models.CharField(max_length=50, choices=CATEGORIA_CHOICES)
+
+  Legenda = models.TextField()
+
+  Imagem = models.ImageField(upload_to='')
+
+  Data_de_publicação= models.DateTimeField(auto_now_add=True)
+
+  Autor = models.CharField(max_length=75)
+
+  Curtidas = models.IntegerField(default=0)
+
+
+
+  def __str__(self):
+    return f"{self.id} - {self.caption}"
+  
+
+
