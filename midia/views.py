ab4639f8a3cd1dc9968fee7f2533bb9c65113d75
Emanuel <emanuel.gui.santana@gmail.com>
2023-12-01 10:24:27 -0300

    configurando view de index

--- midia/views.py
+++ midia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Midia
 
 def index(request):
-  return render(request, 'midia/index.html')
+    midias_publicadas = Midia.objects.filter(Publicada=True).order_by('-Data_de_publicação')
+    return render(request, 'midia/index.html', {'midias_publicadas': midias_publicadas})
